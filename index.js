4fe16739fb3c5731a15d81b4865f4bb87d308c39
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-07-22 18:53:55 -0700

    :white_check_mark: Test added

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 
 // Start up DB Server
 const { db } = require('./src/auth/models/index.js');
-db.sync().then(() => {
-  // Start the web server
-  require('./src/server.js').start(process.env.PORT);
-});
+db.sync()
+  .then(() => {
+    // Start the web server
+    require('./src/server.js').startup(process.env.PORT);
+  })
+  .catch((e) => {
+    console.error('Could not start server', e.message);
+  });
